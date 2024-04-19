137562f0c36b9bccef09b8254f2dd0448d02fd17
Naykyr Oliveira <naykyr@hotmail.com>
2024-04-19 18:03:01 -0300

    :art: melhorando estrutura do código

--- pokemon.py
+++ pokemon.py
@@ -1,29 +1,11 @@
 class Pokemon:
-    def __init__(self, tipo, especie, level=1, nome=None):
-        self.tipo = tipo
-        self.especie = especie
+    def __init__(self, level=1, nome=None, ataque='Ataque básico'):
         self.level = level
-
-        if nome:
-            self.nome = nome
-        else:
-            self.nome = especie
-
+        self.ataque = ataque
+        self.nome = nome if nome else self.especie
 
     def __str__(self):
         return '{} ({})' .format(self.nome, self.level)
 
-
     def atacar(self, pokemon):
-        print('{} atacou'.format(self.especie, pokemon))
-
-
-class PokemonEletrico(Pokemon):
-    def atacar(self, pokemon):
-        print('{} lançou um raio do trovão em {}' .format(self.especie, pokemon))
-
-
-meu_pokemon = Pokemon('Water', 'Squartle')
-pokemon_amigo = PokemonEletrico('Electric', 'pikachu')
-
-pokemon_amigo.atacar(meu_pokemon)
+        print('{} lançou {} em {}'.format(self.especie, self.ataque, pokemon))
