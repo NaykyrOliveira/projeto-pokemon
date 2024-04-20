029b9398f9fd299d2225fd138e207716cfe2c6ed
Naykyr Oliveira <naykyr@hotmail.com>
2024-04-20 16:42:22 -0300

    melhora estrutura do código

--- pokemons.py
+++ pokemons.py
@@ -1,17 +1,20 @@
-from pokemon_tipos import *
+tipos_pokemon = {
+    'Elétrico': ['Pikachu', 'Raichu', 'Electabuzz', 'Pichu', 'Jolteon'],
+    'Normal': ['Rattata', 'Persian', 'Meowth', 'Eevee', 'Snorlax', 'Lickitung'],
+    'Planta': ['Chikorita', 'Cacnea', 'Sceptile', 'Turtwig', 'Leafeon', 'Meganium'],
+    'Fogo': ['Charmander', 'Charmeleon', 'Arcanine', 'Flareon', 'Entei', 'Cyndaquil'],
+    'Água': ['Squirtle', 'Wartortle', 'Blastoise', 'Magikarp', 'Staryu', 'Poliwag'],
+    'Dragão': ['Dratini', 'Dragonair', 'Dragonite', 'Reshiram', 'Axew', 'Goodra'],
+    'Psíquico': ['Abra', 'Kadabra', 'Alakazam', 'Hypno', 'Mewtwo', 'Meowstic Macho', 'Espeon', 'Mew']
+}
 
-
-class Pikachu(PokemonEletrico):
-    especie = 'Pikachu'
-
-
-class Charmander(PokemonFogo):
-    especie = 'Charmander'
-
-
-class Squirtle(PokemonAgua):
-    especie = 'Squirtle'
-
-
-class Dragonite(PokemonDragao):
-    especie = 'Dragonite'
+for tipo, pokemons in tipos_pokemon.items():
+    for pokemon in pokemons:
+        print(f"""
+class {pokemon}(Pokemon{tipo}):
+    def __init__(self):
+        super().__init__()
+        self.nome = '{pokemon}'
+        self.ataque = 'Ataque do {pokemon}'
+        self.defesa = 'Defesa do {pokemon}'
+    """)
