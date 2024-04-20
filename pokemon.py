1357b63b631998db6255aa0a11724ef872d233af
Naykyr Oliveira <naykyr@hotmail.com>
2024-04-20 16:42:33 -0300

    melhora estrutura do código

--- pokemon.py
+++ pokemon.py
@@ -1,15 +1,29 @@
+import random
+
+
 class Pokemon:
     def __init__(self,
-                 level: int = 1,
+                 level: int = None,
                  nome: str = None,
-                 ataque='Ataque básico'):
+                 especie: str = None,
+                 pokemons: list = None
+                 ):
+        if level:
+            self.level = level
+        else:
+            self.level = random.randint(1, 400)
 
-        self.level = level
-        self.ataque = ataque
-        self.nome = nome if nome else self.especie
+        self.especie = especie
+
+        if nome:
+            self.name = nome
+        elif pokemons:
+            self.name = random.choice(pokemons)
+        else:
+            self.nome = self.especie
 
     def __str__(self):
         return '{} ({})' .format(self.nome, self.level)
 
     def atacar(self, pokemon):
-        print('{} lançou {} em {}'.format(self.especie, self.ataque, pokemon))
+        print('{} lançou {} em {}'.format(self.nome, self.ataque, pokemon))
